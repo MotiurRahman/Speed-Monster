11670d1edd37b882283da07dc162bd98e859d193
RRO (RSL-Md. Motiur Rahman) <RRO@rahimafrooz.com>
2022-09-15 11:03:04 +0600

    Fix column alignment issue.

--- history.js
+++ history.js
@@ -7,7 +7,7 @@ function addHistory(questionText, timeTaken, errorCount) {
   newRow.innerHTML = `
   <h3>${questionText}</h3>
   <div>
-  <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+  <p>You took: <span class="bold">${parseInt(timeTaken)}</span> seconds</p>
   <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
   </div>
   `;
@@ -30,9 +30,9 @@ function displayHistory() {
     newRow.classList.add("card");
 
     newRow.innerHTML = `
-  <h3>${test.questionText}</h3>
-  <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
-    <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
+  <h3 id="hisTitle" class="col">${test.questionText}</h3>
+  <p id="hisTime" class="col">You took: <span class="bold">${parseInt(test.timeTaken)}</span> seconds</p>
+    <p id="hisErrorCount" class="col">You made <span class="bold red">${test.errorCount}</span> mistakes</p>
   `;
 
     histories.appendChild(newRow);
