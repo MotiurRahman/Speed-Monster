74df88d7ddb81f11c7e8a3c9c89d1219a3ef265b
RRO (RSL-Md. Motiur Rahman) <RRO@rahimafrooz.com>
2022-09-15 12:11:49 +0600

    Fix auto-scrolling issue during pressing space bar.

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ const gameOver = () => {
   // show result
   resultModal.innerHTML += `
     <h1>Finished!</h1>
-    <p>You took: <span class="bold">${parseInt(timeTaken)}</span> seconds</p>
+    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
@@ -140,6 +140,6 @@ setInterval(() => {
   document.getElementById("show-time").innerHTML = `${startTime ? parseInt(timeSpent) : 0} seconds`;
 }, 1000);
 
-// window.onkeydown = function (e) {
-//   return e.keyCode !== 32;
-// };
+window.onkeydown = function (e) {
+  return e.keyCode !== 32;
+};
